391d2b5b1f5243e98bca328ed35c110195a32272
Adam Koch <adam@dogtired.org>
2022-05-16 23:58:41 -0500

    joel 2.0

--- joel/main/main.ino
+++ joel/main/main.ino
@@ -1,4 +1,8 @@
-#include "WiFi.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <HTTPUpdateServer.h>
 
 #define PIN_LED 2
 
@@ -11,6 +15,8 @@
 #define ledChannel 0
 #define resolution 8
 
+#define DEBUG_ON1 true
+
 bool direction = true;
 long startTime = millis();
 
@@ -18,7 +24,62 @@ TaskHandle_t PulseTask;
 TaskHandle_t CheckOnLedTask;
 TaskHandle_t FlashReadyTask;
 
-WiFiServer server(80);
+WebServer httpServer(80);
+HTTPUpdateServer httpUpdater;
+
+void handleRoot() {
+//  char temp[400];
+//  int sec = millis() / 1000;
+//  int min = sec / 60;
+//  int hr = min / 60;
+
+  String message = "<html>\
+  <head>\
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
+    <link rel=\"icon\" href=\"data:,\">\
+    <title>Joel</title>\
+    <style>\
+      button {\
+        background-color: #4CAF50;\
+        border: none;\
+        color: white;\
+        padding: 16px 40px;\
+        text-decoration: none;\
+        font-size: 30px;\
+        margin: 2px;\
+        cursor: pointer;\
+      }\
+    </style>\
+  </head>\
+  <body>\
+    <a href=\"/On\"><button>On</button></a>\
+    <a href=\"/Off\"><button>Off</button></a><br>\
+    <a href=\"/Pulse\"><button>Pulse</button></a><br>\
+    <a href=\"/Blink\"><button>Blink</button></a><br>\
+    Click <a href=\"/update\">here</a> to update the firmware.<br>\
+  </body>\
+</html>";
+
+  httpServer.send(200, "text/html", message);
+}
+
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += httpServer.uri();
+  message += "\nMethod: ";
+  message += (httpServer.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += httpServer.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < httpServer.args(); i++) {
+    message += " " + httpServer.argName(i) + ": " + httpServer.arg(i) + "\n";
+  }
+
+  httpServer.send(404, "text/plain", message);
+}
 
 void setup()
 {
@@ -44,14 +105,26 @@ void setup()
         Serial.print(".");
     }
 
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+//  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+//    WiFi.begin(ssid, password);
+//    Serial.println("WiFi failed, retrying.");
+//  }
 
-     WiFi.setSleep(false);
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.println("IP address: ");
+    String localIP = WiFi.localIP().toString();
+    Serial.println(localIP);
+
+    WiFi.setSleep(false);
     
-    server.begin();
+    if (MDNS.begin("esp32")) {
+      Serial.println("mDNS responder started");
+    }
+  
+    httpUpdater.setup(&httpServer);
+    httpServer.begin();
 
     BaseType_t xReturned;
     xReturned = xTaskCreate(
@@ -75,122 +148,100 @@ void setup()
                       NULL,        /* parameter of the task */
                       0,           /* priority of the task */
                       &PulseTask);  /* Task handle to keep track of created task */
-    if( xReturned == pdPASS )
+    if (xReturned == pdPASS)
     {
       Serial.println("PulseTask created");            
       vTaskSuspend(PulseTask); 
     }
 
+
+    MDNS.addService("http", "tcp", 80);
+    
+    httpServer.on("/", handleRoot);
+    httpServer.on("/On", turnOn);
+    httpServer.on("/Off", turnOff);
+    httpServer.on("/Pulse", startPulse);
+    httpServer.on("/Blink", startBlink);
+    httpServer.on("/test.svg", drawGraph);
+    httpServer.on("/inline", []() {
+      httpServer.send(200, "text/plain", "this works as well");
+    });
+    httpServer.onNotFound(handleNotFound);
+    httpServer.begin();
+    Serial.println("HTTP server started");
+
+    Serial.printf("HTTPUpdateServer ready! Open http://%s/update in your browser\n", localIP);
+  
     FlashReady();
 }
 
-void loop(){
-  WiFiClient client = server.available();   // listen for incoming clients
+void loop() {
+  httpServer.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+}
+        
+void turnOn() {
+  Serial.println("turning on");
+  stopBlink();
+  stopPulse();
+  turnOnLed();
+  startTime = millis();  
+  vTaskResume(CheckOnLedTask);
+  handleRoot();
+}
 
-  if (client) {                             // if you get a client,
-    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    String task = "";
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
+void turnOff() {
+  Serial.println("turning off");
+  stopBlink();
+  stopPulse();
+  turnOffLed();
+  vTaskSuspend(CheckOnLedTask);
+  handleRoot();
+}
 
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/On\">here</a> to turn the LED on.<br>");
-            client.print("Click <a href=\"/Off\">here</a> to turn the LED off.<br>");
-            client.print("Click <a href=\"/Pulse\">here</a> to pulse the LED.<br>");
-            client.print("Click <a href=\"/Blink\">here</a> to blink the LED.<br>");
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-        
-        if (currentLine.endsWith("GET /On")) {
-          task = "On";
-        }
-        
-        else if (currentLine.endsWith("GET /Off")) {
-          task = "Off";
-        }
-        
-        else if (currentLine.endsWith("GET /Blink")) {    
-          task = "Blink";
-        }
-        
-        else if (currentLine.endsWith("GET /Pulse")) {  
-          task = "Pulse";
-          
-        }
-      }
-    }
-        
-    if (task == "On") {
-      Serial.println("turning on");
-      ledcWrite(ledChannel, 100);
-      
-      startTime = millis();  
-      vTaskResume(CheckOnLedTask);
-    }
-    else if (task != "") {
-      vTaskSuspend(CheckOnLedTask);
-    }
-    
-    if (task == "Off") {
-      Serial.println("turning off");
-      ledcWrite(ledChannel, 0);
-      vTaskSuspend(CheckOnLedTask);
-    }
-    
-    if (task == "Blink") {
-      Serial.println("turning on blinking");
-      ledcChangeFrequency(ledChannel, blinkFreq, resolution);
-      ledcWrite(ledChannel, 50);
-    }
-    else if (task != "") {
-      Serial.println("turning off blinking");
-      ledcChangeFrequency(ledChannel, freq, resolution);
-    }
-    
-    if (task == "Pulse") {
-      Serial.println("turning on pulsing");   
-      vTaskResume(PulseTask);       
-    }
-    else if (task != "") {
-      Serial.println("turning off pulsing");   
-      vTaskSuspend(PulseTask); 
-    }
-      
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
+void startBlink() {
+  Serial.println("turning on blinking");
+  stopPulse();
+  
+  double frequency = ledcReadFreq(ledChannel);
+  if (frequency != blinkFreq) {
+    if (DEBUG_ON1) Serial.println("setting frequency to " + blinkFreq);
+    ledcChangeFrequency(ledChannel, blinkFreq, resolution);
   }
+  
+  ledcWrite(ledChannel, 50);
+  handleRoot();
+}
+
+void stopBlink() {
+  Serial.println("turning off blinking");
+  double frequency = ledcReadFreq(ledChannel);
+  if (frequency != (double) freq) {
+    if (DEBUG_ON1) Serial.println("setting frequency to " + freq);
+    ledcChangeFrequency(ledChannel, freq, resolution);
+  }
+}
+    
+void startPulse() {
+  Serial.println("turning on pulsing");   
+  stopBlink();
+  vTaskResume(PulseTask);
+  handleRoot();  
+}
+
+void stopPulse() {
+  Serial.println("turning off pulsing");   
+  vTaskSuspend(PulseTask); 
 }
 
 void CheckOnLed( void * pvParameters ){
   for(;;){
     bool ledOn = ledcRead(ledChannel) == 100;
-    Serial.println("ledOn = " + String(ledOn));
+    if (DEBUG_ON1) Serial.println("ledOn = " + String(ledOn));
     long elapsedTime = millis() - startTime;
-    Serial.println("elapsedTime = " + String(elapsedTime));
+    if (DEBUG_ON1) Serial.println("elapsedTime = " + String(elapsedTime));
     if (ledOn && elapsedTime > 60000) {
-      ledcWrite(ledChannel, 0);
+      turnOffLed();
       vTaskSuspend(CheckOnLedTask);
     }
    
@@ -202,7 +253,7 @@ void PulseLed( void * pvParameters ){
   int fadeValue = 0;
 
   for(;;){
-    Serial.println("fadeValue = " + String(fadeValue));
+    if (DEBUG_ON1) Serial.println("fadeValue = " + String(fadeValue));
     if (direction) {
       fadeValue++;
       if (fadeValue > 99) {
@@ -224,13 +275,40 @@ void PulseLed( void * pvParameters ){
 void FlashReady() {
   for(int i = 0; i < 3; i++) {
     for(int i = 0; i < 3; i++) {
-      ledcWrite(ledChannel, 100);
+      turnOnLed();
       delay(90);
-      ledcWrite(ledChannel, 0);
+      turnOffLed();
       delay(90);
     }
     if (i < 2) {
       delay(500);
     }
   }
+}
+
+void turnOnLed() {
+  ledcWrite(ledChannel, 100);
+}
+
+void turnOffLed() {
+  ledcWrite(ledChannel, 0);
+}
+
+
+void drawGraph() {
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x += 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  httpServer.send(200, "image/svg+xml", out);
 }
