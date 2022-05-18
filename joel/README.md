43a10ffe45cf8da9dd80345cb6f7834b84b608ba
Adam Koch <adam@dogtired.org>
2022-05-18 01:31:33 -0500

    joel 3.1

---
+++ joel/README.md
@@ -0,0 +1,12 @@
+# Joel
+
+## Release Notes
+
+### 1.0
+Initial version with LED and basic web server
+
+### 2.0
+Added firmware updating capabilities
+
+### 3.0 
+Added sound
--- joel/main/main.ino
+++ joel/main/main.ino
@@ -4,29 +4,33 @@
 #include <ESPmDNS.h>
 #include <HTTPUpdateServer.h>
 #include "WiFiCredentials.h"
+#include "esp32-hal-log.h"
 
 #define PIN_LED 2
 #define PIN_SPEAKER 4
 
 #define freq 5000
-#define blinkFreq 5
-#define seizureFreq 5
+#define BLINK_FREQ_REGULAR 5
+#define BLINK_FREQ_SEIZURE 10
 #define ledChannel 0
 #define speakerChannel 1
 #define speakerFreq 10000
 #define resolution 8
-#define NOTE_PAUSE_LENGTH 400
+#define NOTE_PAUSE_LENGTH 150
 #define EIGTH_NOTE_LENGTH 200
 
 #define DEBUG_ON1 true
 
-bool direction = true;
+bool pulseDirection = true;
+bool play = false;
 long startTime = millis();
 
 TaskHandle_t PulseTask;
 TaskHandle_t CheckOnLedTask;
 TaskHandle_t FlashReadyTask;
+TaskHandle_t twinkleTask;
 TaskHandle_t sweepSoundTask;
+//TaskHandle_t runningTask;
 
 WebServer httpServer(80);
 HTTPUpdateServer httpUpdater;
@@ -37,6 +41,8 @@ void handleRoot() {
 //  int min = sec / 60;
 //  int hr = min / 60;
 
+  log_v("Entering handleRoot");
+  
   String response = "<html>\
   <head>\
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\
@@ -60,8 +66,8 @@ void handleRoot() {
     <a href=\"/Off\"><button>Off</button></a><br>\
     <a href=\"/Pulse\"><button>Pulse</button></a><br>\
     <a href=\"/Blink\"><button>Blink</button></a><br>\
-    <a href=\"/twinkle\"><button>Play twinkle twinkle</button></a><br>\
-    <a href=\"/sweep\"><button>Sweep sound</button></a><br>\
+    <a href=\"/Seizure\"><button>Seizure</button></a><br>\
+    <a href=\"/twinkle\">Play Twinkle, Twinkle</a><br>\
     Click <a href=\"/update\">here</a> to update the firmware.<br>\
   </body>\
 </html>";
@@ -87,6 +93,35 @@ void handleNotFound() {
   httpServer.send(404, "text/plain", message);
 }
 
+String getStatus(wl_status_t val) {
+  return getStatus((int) val);
+}
+
+String getStatus(int val) {
+  if (val == WL_IDLE_STATUS) {
+    return "IDLE";
+  }
+  else if (val == WL_NO_SSID_AVAIL) {
+    return "NO_SSID_AVAIL";
+  }
+  else if (val == WL_SCAN_COMPLETED) {
+    return "SCAN_COMPLETED";
+  }
+  else if (val == WL_CONNECTED) {
+    return "CONNECTED";
+  }
+  else if (val == WL_CONNECT_FAILED) {
+    return "CONNECT_FAILED";
+  }
+  else if (val == WL_CONNECTION_LOST) {
+    return "CONNECTION_LOST";
+  }
+  else if (val == WL_DISCONNECTED) {
+    return "DISCONNECTED";
+  }
+  return "unknown";
+}
+
 void setup()
 {
     ledcSetup(ledChannel, freq, resolution);
@@ -103,14 +138,37 @@ void setup()
 
     Serial.println();
     Serial.print("Connecting to ");
-    Serial.println(ssid);
+    Serial.print(ssid);
+    Serial.println("...");
 
-    WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+    wl_status_t status = WiFi.begin(ssid, password);
+    
+    log_d("begin status=%s", getStatus(status));
+    int waitForConnectResultStatus = WiFi.waitForConnectResult();
+    log_d("wait for result status=%s", getStatus(status));
+    int reconnectDelay = 10000;
+    while (waitForConnectResultStatus == WL_CONNECT_FAILED) {
+      Serial.print("WiFi failed, retrying in ");
+      Serial.print(reconnectDelay / 1000);
+      Serial.println(" seconds...");
+      
+      delay(reconnectDelay);
+      reconnectDelay *= 2;
+      WiFi.begin(ssid, password);
+      waitForConnectResultStatus = WiFi.waitForConnectResult();
+      log_d("wait for result repeat status=%s", getStatus(waitForConnectResultStatus));
     }
+    
+    status = WiFi.status();
+    log_d("wifi status=%s", getStatus(status));
+    while (status != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      status = WiFi.status();
+      log_d("wifi repeat status=%s", getStatus(status));
+    }
+    Serial.println("status=" + getStatus(status));
 
 //  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
 //    WiFi.begin(ssid, password);
@@ -124,7 +182,8 @@ void setup()
     String localIP = WiFi.localIP().toString();
     Serial.println(localIP);
 
-    WiFi.setSleep(false);
+// power savings?
+//    WiFi.setSleep(false);
     
     if (MDNS.begin("esp32")) {
       Serial.println("mDNS responder started");
@@ -144,7 +203,7 @@ void setup()
                      
     if( xReturned == pdPASS )
     {
-      if (DEBUG_ON1) Serial.println("CheckOnLedTask created");
+      log_d("CheckOnLedTask created");
       vTaskSuspend(CheckOnLedTask); 
     }
   
@@ -157,10 +216,23 @@ void setup()
                       &PulseTask);  /* Task handle to keep track of created task */
     if (xReturned == pdPASS)
     {
-      if (DEBUG_ON1) Serial.println("PulseTask created");
+      log_d("PulseTask created");
       vTaskSuspend(PulseTask); 
     }
 
+    xReturned = xTaskCreatePinnedToCore(
+        playTwinkleTwinkleTaskMethod,   /* Task function. */
+        "twinkleTask",     /* name of task. */
+        10000,       /* Stack size of task */
+        NULL,        /* parameter of the task */
+        0,           /* priority of the task */
+        &twinkleTask,  /* Task handle to keep track of created task */
+        0);          /* pin task to core 0 */
+    if (xReturned == pdPASS)
+    {
+      log_d("twinkleTask created");
+      vTaskSuspend(twinkleTask); 
+    }
 
     MDNS.addService("http", "tcp", 80);
     
@@ -168,7 +240,8 @@ void setup()
     httpServer.on("/On", turnOn);
     httpServer.on("/Off", turnOff);
     httpServer.on("/Pulse", startPulse);
-    httpServer.on("/Blink", startBlink);
+    httpServer.on("/Blink", startBlinkRegular);
+    httpServer.on("/Seizure", startBlinkSeizure);
     httpServer.on("/sweep", sweepSound);
     httpServer.on("/sound2", testSound2);
     httpServer.on("/sound3", testSound3);
@@ -182,12 +255,30 @@ void setup()
     httpServer.begin();
     Serial.println("HTTP server started");
 
-    if (DEBUG_ON1) Serial.printf("HTTPUpdateServer ready! Open http://%s/update in your browser\n", localIP);
+    log_d("HTTPUpdateServer ready! Open http://%s/update in your browser\n", localIP);
   
     FlashReady();
 }
 
 void loop() {
+
+  // TODO: look at WiFi events: https://techtutorialsx.com/2019/08/11/esp32-arduino-getting-started-with-wifi-events/
+  
+  wl_status_t status = WiFi.status();
+  log_v("wifi status=%s", getStatus(status));
+  if (status != WL_CONNECTED) {
+    // reconnect
+    Serial.println("Reconnecting...");
+    status = WiFi.begin(ssid, password);
+  }
+  while (status != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    status = WiFi.status();
+    log_v("wifi repeat status=%s", getStatus(status));
+  }
+  log_v("status=", getStatus(status));
+    
   httpServer.handleClient();
   delay(2);//allow the cpu to switch to other tasks
 }
@@ -206,7 +297,6 @@ void sweepSound() {
       
   handleRoot();
 }
-    
 
 void sweepSoundTaskMethod(void * pvParameters) {
   ledcAttachPin(PIN_SPEAKER, speakerChannel);
@@ -219,7 +309,7 @@ void sweepSoundTaskMethod(void * pvParameters) {
 }
 
 void testSound2() {
-  Serial.println("testing sound 2");
+  log_i("testing sound 2");
   ledcAttachPin(PIN_SPEAKER, speakerChannel);
   delay(250);
   ledcWriteNote(speakerChannel, NOTE_C, 1);
@@ -227,7 +317,7 @@ void testSound2() {
 }
 
 void testSound3() {
-  Serial.println("testing sound 2");
+  log_i("testing sound 3");
   ledcAttachPin(PIN_SPEAKER, speakerChannel);
   delay(250);
   ledcWriteNote(speakerChannel, NOTE_C, 2);
@@ -235,89 +325,89 @@ void testSound3() {
 }
 
 void turnSoundOff() {
-  Serial.println("turning off sound");
+  log_i("turning off sound");
   ledcDetachPin(PIN_SPEAKER);
   handleRoot();
 }
 
 void playTwinkleTwinkle() {
-  Serial.println("playing twinkle twinkle");
+  log_i("playing twinkle twinkle");
   
-  xTaskCreatePinnedToCore(
-    playTwinkleTwinkleTaskMethod,   /* Task function. */
-    "playTwinkleTwinkleTask",     /* name of task. */
-    10000,       /* Stack size of task */
-    NULL,        /* parameter of the task */
-    0,           /* priority of the task */
-    NULL,  /* Task handle to keep track of created task */
-    0);          /* pin task to core 0 */
-      
+  vTaskResume(twinkleTask);
+  play = true;
+  
   handleRoot();
 }
     
 void playTwinkleTwinkleTaskMethod(void * pvParameters) {
-  ledcAttachPin(PIN_SPEAKER, speakerChannel);
-  delay(250);
-  vTaskResume(PulseTask);
-  delay(250);
-  playQuarter(NOTE_C);
-  playQuarter(NOTE_C);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_A);
-  playQuarter(NOTE_A);
-  playHalf(NOTE_G);
-  delay(NOTE_PAUSE_LENGTH);
+
+  for (;;) {
+    if (play) {
+      ledcAttachPin(PIN_SPEAKER, speakerChannel);
+      delay(250);
+      vTaskResume(PulseTask);
+      delay(250);
   
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_D);
-  playQuarter(NOTE_D);
-  playHalf(NOTE_C);
-  delay(NOTE_PAUSE_LENGTH);
-  
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_E);
-  playHalf(NOTE_D);
-  delay(NOTE_PAUSE_LENGTH);
-  
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_E);
-  playHalf(NOTE_D);
-  delay(NOTE_PAUSE_LENGTH);
-  
-  playQuarter(NOTE_C);
-  playQuarter(NOTE_C);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_G);
-  playQuarter(NOTE_A);
-  playQuarter(NOTE_A);
-  playHalf(NOTE_G);
-  delay(NOTE_PAUSE_LENGTH);
-  
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_F);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_E);
-  playQuarter(NOTE_D);
-  playQuarter(NOTE_D);
-  playHalf(NOTE_C);
-  
-  ledcDetachPin(PIN_SPEAKER);
-  vTaskSuspend(PulseTask); 
-  turnOffLed();
-  
-  handleRoot();
+      playQuarter(NOTE_C);
+      playQuarter(NOTE_C);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_A);
+      playQuarter(NOTE_A);
+      playHalf(NOTE_G);
+      delay(NOTE_PAUSE_LENGTH);
+      
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_D);
+      playQuarter(NOTE_D);
+      playHalf(NOTE_C);
+      delay(NOTE_PAUSE_LENGTH);
+      
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_E);
+      playHalf(NOTE_D);
+      delay(NOTE_PAUSE_LENGTH);
+      
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_E);
+      playHalf(NOTE_D);
+      delay(NOTE_PAUSE_LENGTH);
+      
+      playQuarter(NOTE_C);
+      playQuarter(NOTE_C);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_G);
+      playQuarter(NOTE_A);
+      playQuarter(NOTE_A);
+      playHalf(NOTE_G);
+      delay(NOTE_PAUSE_LENGTH);
+      
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_F);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_E);
+      playQuarter(NOTE_D);
+      playQuarter(NOTE_D);
+      playHalf(NOTE_C);
+      
+      ledcDetachPin(PIN_SPEAKER);
+      vTaskSuspend(PulseTask); 
+      turnOffLed();
+      play = false;
+    }
+    delay(200);
+  }
 }
 
 void playQuarter(note_t note) {
@@ -336,7 +426,7 @@ void playNote(note_t note, int len) {
 }
 
 void turnOn() {
-  Serial.println("turning on");
+  log_i("turning on");
   stopBlink();
   stopPulse();
   turnOnLed();
@@ -346,22 +436,40 @@ void turnOn() {
 }
 
 void turnOff() {
-  Serial.println("turning off");
+  log_i("turning off");
   stopBlink();
   stopPulse();
+  log_v("About to turn off LED");
   turnOffLed();
+  log_v("About to suspend CheckOnLedTask");
   vTaskSuspend(CheckOnLedTask);
+  
+  // TODO: get twinkleTask state. if running, stop. suspending task doesn't work. add flag to check?
+  log_v("About to detach speaker");
+  ledcDetachPin(PIN_SPEAKER);
+  log_v("About to suspend twinkleTask");
+  vTaskSuspend(twinkleTask);
+  
+  log_v("About to call handleRoot");
   handleRoot();
 }
 
-void startBlink() {
-  Serial.println("turning on blinking");
+void startBlinkRegular() {
+  startBlink(BLINK_FREQ_REGULAR);
+}
+
+void startBlinkSeizure() {
+  startBlink(BLINK_FREQ_SEIZURE);
+}
+
+void startBlink(int blinkFrequency) {
+  log_i("turning on blinking at rate of %d", blinkFrequency);
   stopPulse();
   
   double frequency = ledcReadFreq(ledChannel);
-  if (frequency != blinkFreq) {
-    if (DEBUG_ON1) Serial.println("setting frequency to " + blinkFreq);
-    ledcChangeFrequency(ledChannel, blinkFreq, resolution);
+  if (frequency != blinkFrequency) {
+    log_d("setting frequency to %d", blinkFrequency);
+    ledcChangeFrequency(ledChannel, blinkFrequency, resolution);
   }
   
   ledcWrite(ledChannel, 50);
@@ -369,32 +477,37 @@ void startBlink() {
 }
 
 void stopBlink() {
-  Serial.println("turning off blinking");
+  log_i("turning off blinking");
   double frequency = ledcReadFreq(ledChannel);
-  if (frequency != (double) freq) {
-    if (DEBUG_ON1) Serial.println("setting frequency to " + freq);
+  log_d("frequency=%d", frequency);
+  log_d("freq=%d", freq);
+  log_d("(int) frequency != freq=%d", (int) frequency != freq);
+  if ((int) frequency != freq) {
+    log_d("setting frequency to %d", freq);
     ledcChangeFrequency(ledChannel, freq, resolution);
   }
+  log_d("exiting stopBlink");
 }
     
 void startPulse() {
-  Serial.println("turning on pulsing");   
+  log_i("turning on pulsing");   
   stopBlink();
   vTaskResume(PulseTask);
   handleRoot();  
 }
 
 void stopPulse() {
-  Serial.println("turning off pulsing");   
+  log_i("turning off pulsing");   
   vTaskSuspend(PulseTask); 
+  log_d("exiting stopPulse");
 }
 
 void CheckOnLed( void * pvParameters ){
   for(;;){
     bool ledOn = ledcRead(ledChannel) == 100;
-    if (DEBUG_ON1) Serial.println("ledOn = " + String(ledOn));
+    log_d("ledOn=%b", ledOn);
     long elapsedTime = millis() - startTime;
-    if (DEBUG_ON1) Serial.println("elapsedTime = " + String(elapsedTime));
+    log_d("elapsedTime=%d", elapsedTime);
     if (ledOn && elapsedTime > 60000) {
       turnOffLed();
       vTaskSuspend(CheckOnLedTask);
@@ -408,17 +521,17 @@ void PulseLed( void * pvParameters ){
   int fadeValue = 0;
 
   for(;;){
-    if (DEBUG_ON1) Serial.println("fadeValue = " + String(fadeValue));
-    if (direction) {
+    log_v("fadeValue=%d", fadeValue);
+    if (pulseDirection) {
       fadeValue++;
       if (fadeValue > 99) {
-        direction = false;
+        pulseDirection = false;
       }
     }
     else {
       fadeValue--;
       if (fadeValue < 1) {
-        direction = true;
+        pulseDirection = true;
       }
     }
     
